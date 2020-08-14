4fd662f182e6fdb6278840efff2cee333a560372
Jason <jxun98@gmail.com>
2020-08-13 23:01:11 -0700

    The bot tweets back after a mention.

--- src/bot.js
+++ src/bot.js
@@ -4,3 +4,20 @@ var Twit = require('twit');
 const config = require('../config.json');
 
 var T = new Twit(config);
+
+// Setting up user stream.
+var stream = T.stream('statuses/filter', {track: '@FoodGuruBot'});
+
+stream.on('tweet', followed);
+
+function followed(eventMessage) {
+  console.log('We got a tweet!');
+  console.log(eventMessage);
+  let screenName = eventMessage.user.screen_name;
+  let message = '@' + screenName + ' What\'s good my guy?';
+  sendTweet(message);
+}
+
+function sendTweet(tweet) {
+  T.post('statuses/update', {status: tweet});
+}
